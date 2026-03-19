789ccf67d1dc940a8673d80e30c995c6f175b334
gigi16040 <giovannasouzamota1604@gmail.com>
2026-03-19 15:08:21 -0300

    Add files via upload

---
+++ ExemplodeHoje.java
@@ -0,0 +1,12 @@
+
+public class ExemplodeHoje {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+       Pessoa rafaella = new Pessoa ();
+       System.out.print(rafaella.nome);
+       rafaella.recuperaNome;
+    }
+}
